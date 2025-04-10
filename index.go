9b568754c0ee7ce558d81abb4e7c19a0c4a45756
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-04-10 22:57:12 +0530

    initial commit

---
+++ index.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("Suntharalingam Paranitharan")
+
+	age := 245
+	fmt.Println("My age is", age)
+	i:=1
+	for i <= 10{
+		fmt.Println(i)
+		i++
+	}
+	
+
+
+}
